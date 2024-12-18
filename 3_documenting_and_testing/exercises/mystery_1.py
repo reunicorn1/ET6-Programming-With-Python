c882e0fd523a0d2483eb4a1efbdfaecd7d1348ce
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-12-18 17:13:40 +0400

    Practicing on documentation and testing

--- 3_documenting_and_testing/exercises/mystery_1.py
+++ 3_documenting_and_testing/exercises/mystery_1.py
@@ -1,2 +1,28 @@
-def mystery_1(a,b):
+#!/usr/bin/env python3
+"""
+mystery_1
+
+@author: Reem Osama
+"""
+
+
+def mystery_1(a: any, b: any) -> any:
+    """
+    The function calculates the sum of two numbers or appends
+
+    Parameters:
+        a: int, first number
+        b: int, second number
+
+    Returns -> int, the sum of the calculation or the sum of appending
+
+    >>> mystery_1(1, 2)
+    3
+
+    >>> mystery_1(4, 7)
+    11
+
+    >>> mystery_1(-1, 38)
+    37
+    """
     return a + b
--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -1,6 +1,28 @@
-import unittest
+#!/usr/bin/env python3
+"""
+testing mystery_1
 
+@author: Reem Osama
+"""
+
+import unittest
 from ..mystery_1 import mystery_1
 
 class TestMystery1(unittest.TestCase):
-    """ """
+    """ Test the mystery_1 function"""
+    def test_0(self):
+        """it should evaluate 1, 2 to 3"""
+        self.assertEqual(mystery_1(1, 2), 3)
+
+    def test_1(self):
+        """it should evaluate 5, -6 to -1"""
+        self.assertEqual(mystery_1(5, -6), -1)
+
+    def test_3(self):
+        """It should evaluate my, self to myself"""
+        self.assertEqual(mystery_1('my', 'self'), 'myself')
+
+    def test_4(self):
+        """it should evaluate [1, 2], [3] to [1, 2, 3]"""
+        self.assertEqual(mystery_1([1, 2], [3]), [1, 2, 3])
+
